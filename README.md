4d3866447646365d661be9eeb84aac370d74dcc9
Tobias Hutterer <tobias.hutterer@gmail.com>
2016-12-18 17:38:18 +0100

    add package.json instead of installs in Dockerfile

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A Simple dockerized websocket server and a corresponding webapp in SAP UI5
 
 #For Development & Debugging
-docker-compose -f docker-compose.dev.yml up -d
+docker-compose up -d
 
 #For hosting
 docker-compose -f docker-compose.prod.yml up -d
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8080 });
+var WebSocketServer = require('ws').Server, 
+    wss = new WebSocketServer({ port: 8080 });
 
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
@@ -13,6 +13,7 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    
 
     //sende die Nachricht NUR an den sendenden Client
     //ws.send('received:' + message);
