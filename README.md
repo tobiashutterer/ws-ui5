333edb2e084ff0484801e50989212801f91e3311
Tobias Hutterer <tobias.hutterer@gmail.com>
2016-12-18 16:34:56 +0100

    init repo with websocket server part

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ws-ui5
 A Simple dockerized websocket server and a corresponding webapp in SAP UI5
+
+#For Development & Debugging
+docker-compose -f docker-compose.dev.yml up -d
+
+#For hosting
+docker-compose -f docker-compose.prod.yml up -d
---
+++ server/server.js
@@ -0,0 +1,29 @@
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: 8080 });
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
+wss.on('connection', function connection(ws) {
+  console.log('client: %s', 'connected');
+  ws.send('connected');
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    //sende die Nachricht NUR an den sendenden Client
+    //ws.send('received:' + message);
+
+    //sende die Nachricht an alle Client
+    wss.clients.forEach(function each(client) {
+      client.send('received me from a client:' + message);
+    });
+
+  });
+  ws.on('close', function close() {
+    console.log('client: %s', 'disconnected');
+  });
+});
