832a46bc49991061f0cad1ee6de458e9dd72aeb0
Tobias Hutterer <tobias.hutterer@gmail.com>
2016-12-19 14:19:38 +0100

    webapp + chrome remote debugging

--- ui/server.js
+++ ui/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
-var oneDay = 86400000;
+//var oneDay = 86400000;
+var oneDay = 1;
 
 app.use(express.compress());
 
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-app.listen(process.env.PORT || 3000);
-console.log('webserver %s', 'listening on port 3000');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('webserver %s', 'listening on port 3000');
+});
