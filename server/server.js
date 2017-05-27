e8b14df0100fa36c7579c39a287ef598c153c39e
Tobias Hutterer <tobias.hutterer@gmail.com>
2017-05-27 20:14:49 +0200

    edit

--- server/server.js
+++ server/server.js
@@ -1,48 +1,26 @@
-"use strict"; 
+"use strict";
 
 var WebSocketServer = require('ws').Server,
   os = require('os'),
-  wss = new WebSocketServer({ port: 4000 });
+  wss = new WebSocketServer({
+    port: 4000
+  });
 
 
-wss.broadcast = function (data) {
+wss.broadcast = (data) => {
   for (var i in this.clients)
     this.clients[i].send(data);
   console.log('broadcasted: %s', data);
 };
 
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
     console.log('received FLO: %s', message);
     wss.broadcast(message);
   });
-  ws.send(JSON.stringify(
-    { user: 'Server: ', text: 'Welcome to my container ' + os.hostname() }
-  ));
+
+  ws.send(JSON.stringify({
+    user: 'Server: ',
+    text: 'Welcome to my container ' + os.hostname()
+  }));
 });
-/*
-wss.on('connection', function connection(ws) {
-  console.log('client: %s', 'connected');
-  ws.send('connected on container: ' + os.hostname());
-//ws.send('container stats: ' + JSON.stringify(os.cpus(), null, 2));
-
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    
-
-    //sende die Nachricht NUR an den sendenden Client
-    //ws.send('received:' + message);
-
-    //sende die Nachricht an alle Client
-    wss.clients.forEach(function each(client) {
-      client.send('received from a client:' + message);      
-    });
-
-  });
-  ws.on('close', function close() {
-    console.log('client: %s', 'disconnected');
-  });
-});
-*/
-
-
